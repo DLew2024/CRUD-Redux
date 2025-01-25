ef8c951c3277302dc64fcdc89e749053c8c66dde
DLew <96327749+DLew2024@users.noreply.github.com>
2025-01-24 19:04:53 -0600

    Folder change

--- Todo-App/src/state/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-
-import UserReducer from './reducers/user/userReducer';
-
-export const store = configureStore({
-  reducer: {
-    users: UserReducer,
-  },
-});
